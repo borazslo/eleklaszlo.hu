19145815ad54e6c52d3554d1a37b2ff25ff651c6
borazslo <eleklaszlosj@gmail.com>
2019-12-28 14:54:35 +0100

    x

--- .github/actions/build-dist-site/entrypoint.sh
+++ .github/actions/build-dist-site/entrypoint.sh
@@ -49,7 +49,7 @@ git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
 
-echo "${SSH_PRIVATE_KEY}" | sed "s/BEGIN RSA PRIVATE KEY-----/BEGIN----- /" | sed "s/-----END RSA PRIVATE KEY/ -----END/" | tr [:space:] '\n' | sed "s/BEGIN/BEGIN RSA PRIVATE KEY/" | sed "s/END/END RSA PRIVATE KEY/"  > proba.txt
+echo "${SSH_PRIVATE_KEY}" | sed "s/BEGIN RSA PRIVATE KEY-----/BEGIN-----/" | sed "s/-----END RSA PRIVATE KEY/-----END/" | tr [:space:] '\n' | sed "s/BEGIN/BEGIN RSA PRIVATE KEY/" | sed "s/END/END RSA PRIVATE KEY/"  > proba.txt
 echo "${SSH_PRIVATE_KEY}" > proba2.txt
 chmod 600 proba.txt
 chmod 600 proba2.txt
@@ -73,8 +73,8 @@ echo "And pushing to eleklaszlo.hu..."
 
 #eval "$(ssh-agent -s)"
 #
-echo"Próba 1"
-ssh-agent bash -c 'ssh-add proba.txt; git push --force ssh://eleklaszlo@eleklaszlo.hu/home/eleklaszlo/eleklaszlo.git master:master'
+#echo "Próba 1"
+#ssh-agent bash -c 'ssh-add proba.txt; git push --force ssh://eleklaszlo@eleklaszlo.hu/home/eleklaszlo/eleklaszlo.git master:master'
 echo "Próba 2"
 ssh-agent bash -c 'ssh-add proba2.txt; git push --force ssh://eleklaszlo@eleklaszlo.hu/home/eleklaszlo/eleklaszlo.git master:master'
 
